c75555002d2de7be1805029adde8c53a03cbf345
plaguer <101288516+VillainsRule@users.noreply.github.com>
2024-03-02 15:03:19 -0500

    handle `GM_info is not defined`

--- StateFarmClient.js
+++ StateFarmClient.js
@@ -84,6 +84,8 @@
 // @updateURL https://update.greasyfork.org/scripts/482982/StateFarm%20Client%20V3%20-%20Combat%2C%20Bloom%2C%20ESP%2C%20Rendering%2C%20Chat%2C%20Automation%2C%20Botting%2C%20Unbanning%20and%20more.meta.js
 // ==/UserScript==
 
+if (typeof GM_info === 'undefined') GM_info = {};
+
 let attemptedInjection = false;
 console.log("StateFarm: running (before function)");
 
